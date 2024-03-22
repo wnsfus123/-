910f7e4fe64187f291be655793e6b28774931e2e
naleun <wnsfus123@naver.com>
2024-03-22 17:10:18 +0900

    Connect to Node server

---
+++ server.js
@@ -0,0 +1,23 @@
+﻿// 라이브러리 불러오기
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const http = require('http').createServer(app);
+
+// 8080번 포트에서 서버를 실행할거야
+http.listen(8080, () => {
+  // 서버가 정상적으로 실행되면 콘솔창에 이 메시지를 띄워줘
+  console.log("Listening on http://localhost:8080/");
+});
+
+app.use(express.static(path.join(__dirname, '/build')));
+app.get("/api", (req, res) => {
+  res.json({ title: "Hello" });
+});
+
+// 메인페이지 접속 시 build 폴더의 index.html 보내줘
+app.get('/', (res, req) => {
+  req.sendFile(path.join(__dirname, '/build/index.html'));
+})
+
--- src/index.js
+++ src/index.js
@@ -16,3 +16,7 @@ root.render(
 
 // 웹 성능 측정을 위해 reportWebVitals 함수를 사용합니다.
 reportWebVitals();
+
+// 지금 상태 npm start 로는 정상작동 나중에 node 연결해서 db 어쩌구 해야할꺼같아서 설치하고
+// node Server.js 하면 좆창남
+
