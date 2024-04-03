5e1dcf160569eb68e673a3a69b059f1540f9780e
naleun <sion_1@naver.com>
2024-04-03 13:18:08 +0900

    mysql 적용
    임시 연결 테스트 완료

--- server.js
+++ server.js
@@ -1,23 +1,67 @@
-﻿// 라이브러리 불러오기
-const express = require('express');
+﻿const express = require('express');
 const path = require('path');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const moment = require('moment');
 const app = express();
-
 const http = require('http').createServer(app);
 
-// 8080번 포트에서 서버를 실행할거야
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'mysql'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
 http.listen(8080, () => {
-  // 서버가 정상적으로 실행되면 콘솔창에 이 메시지를 띄워줘
   console.log("Listening on http://localhost:8080/");
 });
 
 app.use(express.static(path.join(__dirname, '/build')));
-app.get("/api", (req, res) => {
-  res.json({ title: "Hello" });
+
+app.post("/api/events", (req, res) => {
+  const { eventName, day, time } = req.body;
+
+  // 날짜와 시간 결합하여 datetime 형식으로 변환
+  const eventDateTime = moment(`${day} ${time}`, 'YYYY-MM-DD HH:mm').format('YYYY-MM-DD HH:mm:ss');
+
+  const eventData = {
+    uuid: generateUUID(), // UUID 생성 로직
+    eventname: eventName,
+    day: eventDateTime
+  };
+
+  connection.query('INSERT INTO test SET ?', eventData, (error, results, fields) => {
+    if (error) {
+      console.error('Error inserting event:', error);
+      res.status(500).send('Error inserting event');
+      return;
+    }
+    console.log('Event added successfully');
+    console.log('Event Name:', eventName);
+    console.log('day:', eventDateTime);
+    
+    res.status(200).send('Event added successfully');
+  });
 });
 
-// 메인페이지 접속 시 build 폴더의 index.html 보내줘
-app.get('/', (res, req) => {
-  req.sendFile(path.join(__dirname, '/build/index.html'));
-})
 
+function generateUUID() {
+  // Implement your UUID generation logic here
+  // For simplicity, you can use libraries like 'uuid' or generate a UUID manually
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/build/index.html'));
+});
