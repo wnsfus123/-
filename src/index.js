e2e9b3b5df949b7ebe47c5b1a8cf70b8836bb816
naleun <wnsfus123@naver.com>
2024-04-04 15:01:58 +0900

    react-dom-routing TEST
    Eventpage.js 추가 react-dom-routing 설치

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './index.css';
-import { createRoot } from 'react-dom/client';
-import App from './App'; // 메인 컴포넌트를 임포트합니다.
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; // BrowserRouter를 사용하여 라우팅 설정
+import App from './App';
 
-const root = createRoot(document.getElementById('root')); // 'root' ID를 가진 DOM 요소를 선택합니다.
-root.render(<App />); // 메인 컴포넌트를 렌더링합니다.
-
-// ReactDOM.render() 메서드는 제거합니다.
-
-// 웹 성능 측정을 위해 reportWebVitals 함수를 사용합니다.
-
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
