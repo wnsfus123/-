0b851de1fcc20ce340fc618f0e213b879a498ae5
장경민 <rudals6383@naver.com>
2024-04-05 14:00:37 +0900

    동적 라우팅

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@
 
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import EventPage from "./EventPage"; // EventPage.js 파일을 가져옵니다.
+import axios from 'axios';
+import { Form, Input, Button, DatePicker, TimePicker } from 'antd';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -34,24 +37,24 @@ class App extends React.Component {
 
   handleConfirm() {
     const { startTime, endTime, selectedDates, eventName } = this.state;
-
+  
     // 최소한 두 개 이상의 날짜가 선택되었는지 확인
     if (selectedDates.length < 2) {
       console.error("At least two dates should be selected");
       return;
     }
-
+  
     // 첫 번째와 두 번째 날짜 선택
     const startDay = selectedDates[0];
     const endDay = selectedDates[1];
-
+  
     // 시작 및 종료 시간 문자열 생성
     const startTimeStr = startTime.format("HH:mm");
     const endTimeStr = endTime.format("HH:mm");
     // 클라이언트의 시간대로 변환하여 서버로 전송
     const startDayLocal = startDay.format("YYYY-MM-DD");
     const endDayLocal = endDay.format("YYYY-MM-DD");
-
+  
     // 시작일과 종료일을 서버로 전송
     axios
       .post("/api/events", {
@@ -63,18 +66,22 @@ class App extends React.Component {
       })
       .then((response) => {
         console.log("Data sent successfully:", response.data);
+        const uuid = response.data.uuid; // 서버로부터 받은 UUID
+  
+        // 이벤트 페이지로 리다이렉션
+        window.location.href = `/events/${uuid}`;
       })
       .catch((error) => {
         console.error("Error sending data:", error);
       });
-
+  
     console.log("Event Name:", eventName);
     console.log("Start Day:", startDay);
     console.log("End Day:", endDay);
     console.log("Selected Start Time:", startTimeStr);
     console.log("Selected End Time:", endTimeStr);
   }
-
+  
   render() {
     return (
       <Router>
@@ -136,6 +143,7 @@ class App extends React.Component {
           </Route>
         </Switch>
       </Router>
+
     );
   }
 }
