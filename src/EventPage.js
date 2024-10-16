8555ce4c78b710042977142203bf9956c51e8196
naleun <wnsfus123@naver.com>
2024-10-17 03:14:57 +0900

    Update EventPage.js

--- src/EventPage.js
+++ src/EventPage.js
@@ -5,7 +5,7 @@ import { Button, Card, Typography, Row, Col, message, Tooltip } from "antd";
 import ScheduleSelector from "react-schedule-selector";
 import { checkKakaoLoginStatus, getUserInfoFromLocalStorage, clearUserInfoFromLocalStorage } from './Components/authUtils';
 import Socialkakao from "./Components/Socialkakao";
-import KakaoShareButton from "./Components/KakaoShareButton"; // KakaoShareButton import
+import KakaoShareButton from "./Components/KakaoShareButton";
 import './App.css';
 
 const { Title, Text } = Typography;
@@ -19,8 +19,8 @@ function EventPage() {
   const [allSchedules, setAllSchedules] = useState([]);
   const [userSchedules, setUserSchedules] = useState({});
   const [userInfo, setUserInfo] = useState(null);
-  const [userSelectedTimes, setUserSelectedTimes] = useState([]); // 현재 접속한 유저의 이벤트 시간을 저장
-  const [confirmLoading, setConfirmLoading] = useState(false); // Confirm 버튼 비활성화 상태 관리
+  const [userSelectedTimes, setUserSelectedTimes] = useState([]);
+  const [confirmLoading, setConfirmLoading] = useState(false);
 
   useEffect(() => {
     const checkLoginStatus = async () => {
@@ -42,146 +42,100 @@ function EventPage() {
     checkLoginStatus();
   }, []);
 
-  useEffect(() => {
-    const fetchEventData = async () => {
-      try {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const uuid = urlParams.get("key");
+  const fetchEventData = async () => {
+    try {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const uuid = urlParams.get("key");
 
-        const response = await axios.get(`/api/events/${uuid}`);
-        setEventData(response.data);
+      const response = await axios.get(`/api/events/${uuid}`);
+      setEventData(response.data);
 
-        const startDate = moment(response.data.startday);
-        const endDate = moment(response.data.endday);
-        const diffDays = endDate.diff(startDate, "days") + 1;
-        setNumDays(diffDays);
+      const startDate = moment(response.data.startday);
+      const endDate = moment(response.data.endday);
+      const diffDays = endDate.diff(startDate, "days") + 1;
+      setNumDays(diffDays);
 
-        const schedulesResponse = await axios.get(`/api/event-schedules/${uuid}`);
-        setAllSchedules(schedulesResponse.data);
+      const schedulesResponse = await axios.get(`/api/event-schedules/${uuid}`);
+      setAllSchedules(schedulesResponse.data);
 
-        const userSchedulesMap = {};
-        schedulesResponse.data.forEach(schedule => {
-          const time = moment(schedule.event_datetime).format("YYYY-MM-DD HH:mm");
-          if (!userSchedulesMap[time]) {
-            userSchedulesMap[time] = [];
-          }
-          userSchedulesMap[time].push(schedule.nickname);
-        });
-        setUserSchedules(userSchedulesMap);
-
-        console.log('User Info:', userInfo); // 사용자 정보 확인
-        console.log('Schedules Response:', schedulesResponse.data); // 전체 스케줄 확인
-
-        // User's existing schedule
-        if (userInfo) {
-          const userSchedule = schedulesResponse.data.filter(schedule => schedule.kakaoId === userInfo.id.toString() && schedule.event_uuid === uuid);
-          console.log('User Schedule:', userSchedule); // 사용자 스케줄 확인
-
-          const userSelectedTime = userSchedule.map(schedule => moment(schedule.event_datetime).toDate());
-          setSchedule(userSelectedTime);
-          setUserSelectedTimes(userSchedule.map(schedule => moment(schedule.event_datetime).format("YYYY-MM-DD HH:mm"))); // 현재 접속한 유저의 이벤트 시간 설정
-          // Set selectedTime for the handleConfirm function
-          const selectedTimeByDate = {};
-          userSelectedTime.forEach((time) => {
-            const date = moment(time).format("YYYY-MM-DD");
-            if (!selectedTimeByDate[date]) {
-              selectedTimeByDate[date] = [];
-            }
-            selectedTimeByDate[date].push(moment(time).format("HH:mm"));
-          });
-          setSelectedTime(selectedTimeByDate);
+      const userSchedulesMap = {};
+      schedulesResponse.data.forEach(schedule => {
+        const time = moment(schedule.event_datetime).format("YYYY-MM-DD HH:mm");
+        if (!userSchedulesMap[time]) {
+          userSchedulesMap[time] = [];
         }
+        userSchedulesMap[time].push(schedule.nickname);
+      });
+      setUserSchedules(userSchedulesMap);
 
-      } catch (error) {
-        console.error("Error fetching event data:", error);
-        message.error("Error fetching event data");
-      } finally {
-        setLoading(false);
+      if (userInfo) {
+        const userSchedule = schedulesResponse.data.filter(schedule => schedule.kakaoId === userInfo.id.toString() && schedule.event_uuid === uuid);
+
+        const userSelectedTime = userSchedule.map(schedule => moment(schedule.event_datetime).toDate());
+        setSchedule(userSelectedTime);
+        setUserSelectedTimes(userSchedule.map(schedule => moment(schedule.event_datetime).format("YYYY-MM-DD HH:mm")));
+
+        const selectedTimeByDate = {};
+        userSelectedTime.forEach((time) => {
+          const date = moment(time).format("YYYY-MM-DD");
+          if (!selectedTimeByDate[date]) {
+            selectedTimeByDate[date] = [];
+          }
+          selectedTimeByDate[date].push(moment(time).format("HH:mm"));
+        });
+        setSelectedTime(selectedTimeByDate);
       }
-    };
 
+    } catch (error) {
+      console.error("Error fetching event data:", error);
+      message.error("Error fetching event data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchEventData();
-  }, [userInfo]); // userInfo가 변경될 때마다 fetchEventData를 호출합니다.
+  }, [userInfo]);
 
   const handleConfirm = async () => {
     try {
-      setConfirmLoading(true); // Confirm 버튼 비활성화
-  
-      // 기존 일정을 삭제
+      setConfirmLoading(true);
+
       await axios.delete("/api/delete-event-schedule", {
         data: {
           kakaoId: userInfo.id.toString(),
           event_uuid: eventData.uuid,
         },
       });
-  
-      const newSchedules = []; // 새로운 스케줄을 저장할 배열
-  
-      // 새로운 일정을 저장
-    for (const [date, times] of Object.entries(selectedTime)) {
-      for (const time of times) {
-        const datetime = moment(`${date} ${time}`, "YYYY-MM-DD HH:mm").format();
 
-        const requestData = {
-          kakaoId: userInfo.id.toString(),
-          nickname: userInfo.kakao_account.profile.nickname,
-          event_name: eventData.eventname,
-          event_uuid: eventData.uuid,
-          event_datetime: datetime,
-        };
-  
-          await axios.post("/api/save-event-schedule", requestData);
-  
-          // 새로 선택된 시간을 배열에 추가하여 즉시 업데이트에 사용
-          newSchedules.push({
+      for (const [date, times] of Object.entries(selectedTime)) {
+        for (const time of times) {
+          const datetime = moment(`${date} ${time}`, "YYYY-MM-DD HH:mm").format();
+
+          const requestData = {
+            kakaoId: userInfo.id.toString(),
+            nickname: userInfo.kakao_account.profile.nickname,
+            event_name: eventData.eventname,
+            event_uuid: eventData.uuid,
             event_datetime: datetime,
-            nickname: userInfo.kakao_account.profile.nickname
-          });
+          };
+
+          await axios.post("/api/save-event-schedule", requestData);
         }
       }
-  
-      // 모든 스케줄이 성공적으로 저장된 후, UI에 바로 반영
-      const updatedSchedules = [...allSchedules, ...newSchedules];
-      setAllSchedules(updatedSchedules); // 전체 스케줄 업데이트
-  
-      // 유저 선택 시간을 업데이트하여 즉시 반영
-      setUserSelectedTimes(newSchedules.map(schedule => moment(schedule.event_datetime).format("YYYY-MM-DD HH:mm")));
-  
-      // 사용자 스케줄을 업데이트하여 새로 선택한 시간이 바로 적용되도록 반영
-      const updatedUserSchedules = { ...userSchedules };
-      newSchedules.forEach((schedule) => {
-        const formattedTime = moment(schedule.event_datetime).format("YYYY-MM-DD HH:mm");
-  
-        // 중복된 사용자 이름을 추가하지 않도록 수정
-        if (!updatedUserSchedules[formattedTime]) {
-          updatedUserSchedules[formattedTime] = [];
-        }
-        
-        // 이미 해당 시간에 사용자의 이름이 포함되어 있는지 확인 후, 추가
-        if (!updatedUserSchedules[formattedTime].includes(schedule.nickname)) {
-          updatedUserSchedules[formattedTime].push(schedule.nickname);
-        }
-      });
-  
-      setUserSchedules(updatedUserSchedules); // UI에서 즉시 반영
-  
+
+      await fetchEventData(); // Fetch latest data
+
       message.success("일정이 즉시 적용되었습니다");
     } catch (error) {
       console.error("일정 저장 오류:", error);
       message.error("일정 저장 오류");
     } finally {
-      setConfirmLoading(false); // Confirm 버튼 다시 활성화
+      setConfirmLoading(false);
     }
   };
-  
-  
-  
-  
-  
-  
-
-
 
   const handleScheduleChange = (newSchedule) => {
     setSchedule(newSchedule);
@@ -273,7 +227,7 @@ function EventPage() {
           <Col span={12}>
             <Card style={{ margin: "20px", padding: "20px" }}>
               <Title level={2}>일정 공유</Title>
-              <KakaoShareButton userInfo={userInfo} eventData={eventData} /> {/* KakaoShareButton 추가 */}
+              <KakaoShareButton userInfo={userInfo} eventData={eventData} />
               <Button type="default" onClick={handleCopyLink} style={{ marginTop: "10px" }}>
                 일정 링크 복사
               </Button>
@@ -306,7 +260,6 @@ function EventPage() {
               <Button type="primary" onClick={handleConfirm} style={{ marginTop: "20px" }} loading={confirmLoading}>
                 확인
               </Button>
-              {/* 선택된 시간 출력 */}
               <Card style={{ margin: "20px", padding: "20px", overflowX: "auto" }}>
                 <Title level={3}>선택된 스케줄 시간</Title>
                 {userSelectedTimes.map((time, index) => (
@@ -338,10 +291,8 @@ function EventPage() {
                     const formattedTime = moment(time).format("YYYY-MM-DD HH:mm");
                     const users = userSchedules[formattedTime] || [];
                     
-                    // 같은 사용자가 여러 번 선택된 경우 중복 이름을 제거
                     const uniqueUsers = [...new Set(users)];
                     
-                    // 사용자 수에 따라 색상 진하기 설정 (중복되면 진해지지 않도록 함)
                     const occurrences = uniqueUsers.length;
                     const opacity = Math.min(0.1 + occurrences * 0.1, 1);
                     
